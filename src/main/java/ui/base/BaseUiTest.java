4fbfc107240f658d8a6bccb5fe46503d03812318
Miziday <kunts.1000@mail.ru>
2026-08-27 11:27:33 +0700

    Тест без remote

--- src/main/java/ui/base/BaseUiTest.java
+++ src/main/java/ui/base/BaseUiTest.java
@@ -22,7 +22,10 @@ public class BaseUiTest {
         Configuration.browser = ConfigReader.get("browser");
         Configuration.browserSize = "1920x1080";
         Configuration.timeout = Long.parseLong(ConfigReader.get("timeout"));
-        Configuration.remote = "http://selenium-chrome:4444/wd/hub";
+//        if (ConfigReader.get("remote.connection.use").equals("true")) {
+//            Configuration.remote = "http://selenium-chrome:4444/wd/hub";
+//        }
+
     }
 
     @AfterMethod(alwaysRun = true)
