cccef6dc4b2f1df89bc9c4011a9b938495f229bd
Miziday <kunts.1000@mail.ru>
2026-09-06 09:56:52 +0700

    Добавил логи после начала и завершения UI тестов

--- src/main/java/ui/base/BaseUiTest.java
+++ src/main/java/ui/base/BaseUiTest.java
@@ -4,6 +4,10 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import config.ConfigReader;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.slf4j.Logger;
+import java.lang.reflect.Method;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -11,8 +15,14 @@ import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class BaseUiTest {
 
+    private static final Logger log = LoggerFactory.getLogger(BaseUiTest.class);
+
     @BeforeMethod
-    public void setup() {
+    public void setup(Method method) {
+
+        log.info("START TEST: {}.{}",
+                method.getDeclaringClass().getSimpleName(),
+                method.getName());
 
         SelenideLogger.addListener("AllureSelenide",
                 new AllureSelenide()
@@ -25,11 +35,25 @@ public class BaseUiTest {
         if (ConfigReader.get("remote.connection.use").equals("true")) {
             Configuration.remote = "http://selenium-chrome:4444/wd/hub";
         }
-
     }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDown() {
+    public void tearDown(ITestResult result) {
+
+        log.info("FINISH TEST: {}.{} | STATUS: {}",
+                result.getTestClass().getRealClass().getSimpleName(),
+                result.getMethod().getMethodName(),
+                getStatus(result));
+
         closeWebDriver();
+    }
+
+    private String getStatus(ITestResult result) {
+        return switch (result.getStatus()) {
+            case ITestResult.SUCCESS -> "PASSED";
+            case ITestResult.FAILURE -> "FAILED";
+            case ITestResult.SKIP -> "SKIPPED";
+            default -> "UNKNOWN";
+        };
     }
 }
